0b68423f1a370461530984bb83dc0e1a0028b2ad
stanislaw <s.pankevich@gmail.com>
2012-06-25 23:58:34 +0300

    presetup

---
+++ ruby/rmagick.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/ruby
+
+require 'rubygems'
+require 'RMagick'
+
+include Magick
+
+puts Magick::Long_version
+
+im = ImageList.new(ARGV[0])
+
+im = im.shave(100, 100)
+im = im.scale(0.9)
+kernel = [-1, -1, -1, -1, 16, -1, -1, -1, -1]
+im = im.convolve(3, kernel)
+
+im.write(ARGV[1])
